dda71773f8e4d846adecc278e0d95cf6ab9c25d6
NguyenArthur <74012763+NguyenArthur@users.noreply.github.com>
2024-01-14 15:27:50 +0100

    fiwind end to end test

--- endToEnd-test.py
+++ endToEnd-test.py
@@ -3,22 +3,29 @@ import pytest
 import time
 import json
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import unittest
 
-class TestEndToEndTest():
-  def setup_method(self, method):
+class TestEndToEnd(unittest.TestCase):
+  def setUp(self):
     self.driver = webdriver.Chrome()
     self.vars = {}
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    self.driver = webdriver.Chrome(options=chrome_options)
   
-  def teardown_method(self, method):
-    self.driver.quit()
+  def tearDown(self):
+        self.driver.quit()
   
-  def test_endToEndTest(self):
+  def test_endToEnd(self):
     self.driver.get("http://localhost:5000/")
     self.driver.set_window_size(1552, 936)
     self.driver.find_element(By.NAME, "item").click()
@@ -36,3 +43,5 @@ class TestEndToEndTest():
     self.driver.find_element(By.LINK_TEXT, "Delete").click()
     self.driver.find_element(By.LINK_TEXT, "Delete").click()
   
+if __name__ == "__main__":
+    unittest.main()
