181e8bb40bf1d3c50a85ce14b671a1ed5aa7faeb
NguyenArthur <74012763+NguyenArthur@users.noreply.github.com>
2024-01-14 19:29:28 +0100

    fixing

--- integration_test/test_endtoend.py
+++ integration_test/test_endtoend.py
@@ -2,9 +2,10 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 
 
-class TestAppE2E(unittest.TestCase):
+class TestEndToEnd(unittest.TestCase):
     def setUp(self):
         options = webdriver.ChromeOptions()
         options.add_argument("--no-sandbox")
@@ -17,24 +18,20 @@ class TestAppE2E(unittest.TestCase):
         self.driver.quit()
 
     def test_add_and_delete_and_update_item(self):
-        self.driver.set_window_size(1406, 860)
         self.driver.get("http://localhost:5000/")
+        self.driver.set_window_size(1552, 936)
         self.driver.find_element(By.NAME, "item").click()
-        self.driver.find_element(By.NAME, "item").send_keys("12")
+        self.driver.find_element(By.NAME, "item").send_keys("test1")
         self.driver.find_element(By.CSS_SELECTOR, "button").click()
         self.driver.find_element(By.NAME, "item").click()
-        self.driver.find_element(By.NAME, "item").send_keys("123")
-        self.driver.find_element(By.CSS_SELECTOR, ".container > form > button").click()
-        self.driver.find_element(By.NAME, "item").click()
-        self.driver.find_element(By.NAME, "item").send_keys("10")
+        element = self.driver.find_element(By.CSS_SELECTOR, ".container > form > button")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element).perform()
+        self.driver.find_element(By.NAME, "item").send_keys("test2")
         self.driver.find_element(By.CSS_SELECTOR, ".container > form > button").click()
         self.driver.find_element(By.NAME, "new_item").click()
-        self.driver.find_element(By.NAME, "new_item").send_keys("100")
-        self.driver.find_element(By.NAME, "new_item").send_keys(Keys.ENTER)
-        self.driver.find_element(By.CSS_SELECTOR, "li:nth-child(2) input").click()
-        self.driver.find_element(By.CSS_SELECTOR, "li:nth-child(2) input").send_keys("14")
-        self.driver.find_element(By.CSS_SELECTOR, "li:nth-child(2) input").send_keys(Keys.ENTER)
-        self.driver.find_element(By.CSS_SELECTOR, "li:nth-child(3) > a").click()
+        self.driver.find_element(By.NAME, "new_item").send_keys("rename test1")
+        self.driver.find_element(By.CSS_SELECTOR, "li:nth-child(1) button").click()
         self.driver.find_element(By.LINK_TEXT, "Delete").click()
         self.driver.find_element(By.LINK_TEXT, "Delete").click()
 
